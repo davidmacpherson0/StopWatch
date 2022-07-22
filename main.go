1794d779c3e96a733e7212b9f1864646461c8a46
David MacPherson <david@tensorworks.com.au>
2022-07-23 09:25:15 +1000

    woking with channels

--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
 func main() {
+
+	running := true
+	for running {
+		running = false
+
+	}
+
+}
+
+func runTimerFor20Sec() {
+	forever := make(chan time.Duration)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go timer(ctx, forever)
+
 	fmt.Println("start")
-	go timer()
+
 	time.Sleep(20 * time.Second)
+	cancel()
+
+	<-forever
 	fmt.Println("\nstoped")
 }
 
-func timer() {
+func timer(ctx context.Context, forever chan time.Duration) {
 	start := time.Now()
 
-	for i := 0; i < 100; i++ {
-		fmt.Printf("%s\r", time.Since(start))
+	for {
+		timevalue := time.Since(start)
+
+		select {
+		case <-ctx.Done():
+			forever <- timevalue
+			return
+		default:
+			fmt.Printf("%s\r", timevalue)
+		}
 		time.Sleep(1 * time.Millisecond)
 	}
-
 }
